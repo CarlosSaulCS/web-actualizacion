46b2316c4f7b2de3ec772bb9c71c2e025694df27
CS <carlos-bugati01@hotmail.com>
2024-12-30 18:43:35 -0600

    anexion con google analitycs

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { WhatsAppButton } from '@/components/WhatsAppButton'
 import { LanguageProvider } from '@/contexts/LanguageContext'
 import { ThemeProvider } from '@/contexts/ThemeContext'
 import { Toaster } from 'sonner'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,6 +17,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" suppressHydrationWarning>
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-0JXP77RRJP`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0JXP77RRJP');
+            `,
+          }}
+        />
+      </head>
       <body className={`${inter.className} flex flex-col min-h-screen`}>
         <ThemeProvider defaultTheme="system" enableSystem>
           <LanguageProvider>
